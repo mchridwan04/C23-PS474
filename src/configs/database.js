421bc062286f029b7cb9895a219c2cc421047321
Mochamad Ridwan <106060496+mchridwan04@users.noreply.github.com>
2023-06-07 12:43:24 +0700

    connect database

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('capstone2023', 'root', '', {
-      host: 'localhost',
+const sequelize = new Sequelize('capstone2023', 'bengkel', '12345678', {
+      host: '34.101.47.38',
       dialect: 'mysql',
 });
 
